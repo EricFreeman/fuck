d4c4b407217b1465aa49931fc81f645ea319735e
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-07-14 23:07:16 +0530

    #4 - Have to give way to CS 6 code.

--- src/fuck/Models/Command.cs
+++ src/fuck/Models/Command.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace fuck.Models
 {
     public class Command
     {
-        public string OriginalCommand { get; set;} 
-
-        public IEnumerable<string> UpdatedCommand;
+        public string OriginalCommand { get; set; }
+        public IEnumerable<string> UpdatedCommand => Recommendations.Select(x => Program + " " + x + " " + Arguments);
 
         public string Program { get; set; }
         public string Action { get; set; }
         public string Arguments { get; set; }
 
-        public IEnumerable<string> Recommendations { get; set; }
-
+        public IEnumerable<string> Recommendations { get; set; } 
     }
 }
