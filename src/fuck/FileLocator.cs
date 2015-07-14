7103cadb0fee6ccf07b7ef0ae14f59fc3b82e120
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-07-15 00:21:39 +0530

    Added a file locator class that will contain and fetch locations for git bash files.

---
+++ src/fuck/FileLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fuck
+{
+    public class FileLocator
+    {
+        private const string BASH_HISTORY_FILE_NAME = @".bash_history";
+        private const string SYSTEM_DRIVE = @"%SystemDrive%";
+        private const string BSLASH = @"\";
+        public string BashHistoryPath()
+        {
+            return WindowsUsersDiractory() + BSLASH + UserDirectory() + BSLASH + BASH_HISTORY_FILE_NAME;
+        }
+        private string WindowsUsersDiractory()
+        {
+            return Environment.ExpandEnvironmentVariables(SYSTEM_DRIVE) + BSLASH + "Users";
+        }
+
+        private string UserDirectory()
+        {
+            return Environment.UserName;
+        }
+    }
+}
